2e0af8d8746fb1d9458995afcf016418d38f4896
George Gastaldi <gegastaldi@gmail.com>
2019-01-14 18:36:16 -0200

    compiles

--- src/main/java/org/acme/quickstart/GreetingService.java
+++ src/main/java/org/acme/quickstart/GreetingService.java
@@ -1,12 +1,20 @@
 package org.acme.quickstart;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.jboss.forge.furnace.Furnace;
 
 @ApplicationScoped
-public class GreetingService {
+public class GreetingService
+{
+   @Inject
+   private Furnace furnace;
 
-    public String greeting(String name) {
-        return "hello " + name;
-    }
+   public String greeting(String name)
+   {
+      System.out.println(Furnace.instance(getClass().getClassLoader()));
+      return "hello " + name + " ";
+   }
 
 }
